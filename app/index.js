edf2e8a6f4798728126fdf469ececc0324043fea
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-21 04:03:57 -0500

    init

---
+++ app/index.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { app, BrowserWindow } = require('electron')
+const devTools = require('./devtools')
+const handleErrors = require('./handle-errors')
+var win = ''
+
+if (process.env.NODE_ENV === 'development') {
+  devTools()
+}
+
+app.on('before-quit', () => {
+
+})
+
+app.on('ready', () => {
+  win = new BrowserWindow({
+    width: 300,
+    height: 400,
+    title: 'hola mundo',
+    maximizable: true,
+    show: false
+  })
+
+  handleErrors(win)
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  win.loadURL(`file://${__dirname}/src/createuser/index.html`)
+
+  win.on('closed', () => {
+    win = null
+    app.quit()
+  })
+})
