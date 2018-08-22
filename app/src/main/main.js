9d1c6a633703c72241fb11b75f7c15bff80a4f6a
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-22 17:24:57 -0500

    main

--- app/src/main/main.js
+++ app/src/main/main.js
@@ -7,28 +7,12 @@ const app = require('electron').remote
 const dialog = app.dialog
 
 window.addEventListener('load', () => {
-  logout()
+  //logout()
 })
 
 
 function logout(){
   document.getElementById('btnLogout').addEventListener('click', function(){
-
-    const BrowserWindow = remote.BrowserWindow
-    const winchild = new BrowserWindow({
-      width: 1200,
-      height: 700,
-      maximizable: true,
-      frame: true,
-      show: false
-    })
-
-    winchild.once('ready-to-show', () => {
-      winchild.show()
-      winchild.focus()
-    })
-
-    winchild.loadURL(`file://${path.join(__dirname, '..')}/main/index.html`)
 
   })
 }
