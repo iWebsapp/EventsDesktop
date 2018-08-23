db61e8f53cad3fef7495bd5f35321a6d4dbf3e97
Luis Castillo <luiscastillo@iwebsapp.com>
2018-08-23 01:48:41 -0500

    nav

--- app/src/controllers/mainController.js
+++ app/src/controllers/mainController.js
@@ -1,39 +1,32 @@
 var eventApp = angular.module('eventApp', ['ngRoute'])
 
 eventApp.config(function($routeProvider) {
-	$routeProvider
-	.when('/', {
-		templateUrl: `../tamplates/main/index.html`,
+	$routeProvider.when('/', {
+		templateUrl: `../tamplates/main/index.html`, //main
 		controller: 'mainCtrl'
-	})
-	.when('/myevents', {
-		templateUrl: `../tamplates/myevents/index.html`,
+	}).when('/myevents', {
+		templateUrl: `../tamplates/myevents/index.html`, //myevents
 		controller: 'myEventCtrl'
-	})
-	.when('/favorites', {
-		templateUrl: `../tamplates/favorites/index.html`,
+	}).when('/favorites', {
+		templateUrl: `../tamplates/favorites/index.html`, //favorites
 		controller: 'favoritesCtrl'
-	})
-	.when('/map', {
-		templateUrl: `../tamplates/map/index.html`,
+	}).when('/map', {
+		templateUrl: `../tamplates/map/index.html`, //map
 		controller: 'mapCtrl'
-	})
-	.when('/profile', {
-		templateUrl: `../tamplates/profile/index.html`,
+	}).when('/profile', {
+		templateUrl: `../tamplates/profile/index.html`, //profile
 		controller: 'profileCtrl'
-	})
-	.when('/settings', {
-		templateUrl: `../tamplates/settings/index.html`,
+	}).when('/settings', {
+		templateUrl: `../tamplates/settings/index.html`, //settings
 		controller: 'settingsCtrl'
-	})
-	.otherwise({
-		templateUrl: `../tamplates/err404/index.html`,
+	}).otherwise({
+		templateUrl: `../tamplates/err404/index.html`, //err404
 		controller: 'err404Ctrl'
 	})
 })
 
 eventApp.controller('mainCtrl', function($scope) {
-	console.log('que onda soy main')
+	console.log('main')
 })
 
 eventApp.controller('myEventCtrl', function($scope) {
@@ -56,12 +49,34 @@ eventApp.controller('profileCtrl', function($scope) {
 	console.log('profiles')
 })
 
-eventApp.controller('logsoutCtrl', function($scope) {
-	console.log('exit')
-})
-
-eventApp.controller('menuCtrl', function($scope) {
-	console.log('menu')
+eventApp.controller('menuCtrl', function($scope, $location) {
+	$scope.btnProfile = function(){
+		$location.path('/profile')
+	}
+	$scope.btnSettings = function(){
+		$location.path('/settings')
+	}
+	$scope.btnLogout = function(){
+		console.log('logout')
+	}
+	$scope.btnHome = function(){
+		$location.path('/')
+	}
+	$scope.btnMyevents = function(){
+		$location.path('/myevents')
+	}
+	$scope.btnFavorites = function(){
+		$location.path('/favorites')
+	}
+	$scope.btnMap = function(){
+		$location.path('/map')
+	}
+	$scope.btnShowNoti = function(num){
+		console.log('show this noti: ', num)
+	}
+	$scope.btnAllNoti = function(){
+		console.log('all noti')
+	}
 })
 
 eventApp.controller('headerCtrl', function($scope) {
